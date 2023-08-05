8c7bb2dde5e91afa94b578d687471e7f997fd443
ruhu <ruhu@ebay.com>
2023-08-05 23:04:09 +0800

    添加google代码跟踪,并开始第一个测试页面

---
+++ docs/_posts/2023-08-05-fisrt-post.markdown
@@ -0,0 +1,10 @@
+---
+layout: default
+title:  "Hello world 测试页面"
+date:   2023-08-05 23:00:00 +0800
+categories: 内容
+tags: [hello world, test]
+author: rd-hu
+---
+这是我的第一个页面，仅用于测试。
+更多精彩内容，尽情期待吧！
---
+++ docs/assets/js/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YMR4JR704B"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YMR4JR704B');
+</script>
