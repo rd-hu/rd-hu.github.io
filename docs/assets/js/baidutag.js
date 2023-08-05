3146adc10be5eecf78023126eb47436ce1dda9b2
ruhu <ruhu@ebay.com>
2023-08-05 23:18:47 +0800

    添加baidu分析

---
+++ docs/assets/js/baidutag.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?16ffb323d63356444b42857eff6b926b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
